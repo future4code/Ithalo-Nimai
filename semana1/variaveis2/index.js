f7b3ac144b0151e20a52336fc289226718b01ae9
Ithalo Nimai <ithalodes@gmail.com>
2021-08-09 15:57:38 -0400

    Programas do dia de hoje

---
+++ semana1/variaveis2/index.js
@@ -0,0 +1,6 @@
+let roupa = prompt("Você está usando uma roupa azul hoje?")
+let tenis = prompt("Você está de tênis?")
+let bermuda = prompt("Você está de bermuda?") 
+
+console.log("Você está usando roupa azul hoje? -", roupa, "Você está de tênis? -", tenis, "Você está de bermuda? -", bermuda)
+//feliz por conseguir resolver
---
+++ semana1/variaveis3/index.js
@@ -0,0 +1,13 @@
+let a = 10
+let b = 25
+let c
+
+ a = c
+ c = b
+ b = c //buguei total e não consegui terminar a tempo kkkkkk
+ 
+
+
+console.log("O novo valor de a é", a)
+console.log("o novo valor de b é", b)
+
