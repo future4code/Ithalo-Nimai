a489dd336d2791c0c396153fd9715f6b85b91f82
Ithalo Nimai <ithalodes@gmail.com>
2021-08-10 15:52:53 -0400

    Operadores da semana1-Aula2

---
+++ semana1-2/operadores3/index.js
@@ -0,0 +1,9 @@
+let suaIdade = Number(prompt("Digite sua idade em anos!"))
+
+const meses = 12
+const dias = 365
+const horas = 8760
+
+console.log(suaIdade * meses)
+console.log(suaIdade * dias)
+console.log(suaIdade * horas)
