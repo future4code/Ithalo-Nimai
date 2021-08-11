196738618f6dc110f250bfa3bd5ca18575b6c246
Ithalo Nimai <ithalodes@gmail.com>
2021-08-11 15:58:36 -0400

    Programas de string e arrays

---
+++ semana1-3/strings-arrays/index.js
@@ -0,0 +1,41 @@
+//>>>Questão1<<<
+//a=undefined b=null c=11 d=3 e=substitui o 4 por 19. f=9 
+
+//>>>Questão2<<<
+//por causa do replaceAll o "A" será trocado por "I". length dará o resultado de 27.
+
+//>>>Atividades escritas<<<
+
+//exercicio 1 =
+
+/* const nomeUsuario = prompt("Qual seu nome?")
+const emailUsuario = prompt("Digite seu email!")
+
+console.log (`O email ${emailUsuario} foi cadastrado com sucesso. Seja bem-vindo(a), ${nomeUsuario}`)
+ */ 
+
+
+//exercicio 2 =
+
+/* array = ["strogonoff", "Pizza", "Sopa de Mandioca", "Churrasco", "Feijoada"]
+console.log("Essas são minhas comidas favoritas", array)
+
+const comida = prompt("Qual sua comida favorita?")
+console.log("A comida favorita do usuário é", comida) */ //nao consegui fazer o desafio.
+
+//exercicio 3 = 
+/* let listaDeTarefas = []
+
+const tarefasDiarias = prompt("Diga 3 tarefas que você precisa realizar em um dia")
+
+console.log(tarefasDiarias)
+
+listaDeTarefas = ["Tomar um banho quando acordar", "almoçar", "lanchar"]
+console.log(listaDeTarefas)
+
+const qualTarefaRealizou = prompt("Indique com 0,1 ou 2 qual dessas ja realizou")
+console.log(qualTarefaRealizou)
+
+listaDeTarefas.splice(0, 1)
+console.log(listaDeTarefas)
+ */
