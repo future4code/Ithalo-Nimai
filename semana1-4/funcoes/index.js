65c942f783561524bc85eda90f2a7bf7fe09e392
Ithalo Nimai <ithalodes@gmail.com>
2021-08-12 15:52:46 -0400

    Exercicios de funções

---
+++ semana1-4/funcoes/index.js
@@ -0,0 +1,60 @@
+//>>>Questão1<<<
+//a. vai ser impresso o resultado de 2 * 5 e 10 * 5
+//b . Não iria aparecer nada
+
+//>>>Questão2<<<
+//a. irá deixar as letras em minusculo e determinar se a string do 
+//usuário será false ou true. 
+//b.
+//I- Ficará tudo minusculo e constará true no console.
+//II- Novamente tudo minúsculo e constará true no console.
+//III- Ficará tudo minusculo e constará false no console.
+
+/* Exercicios de escrita de código
+
+>>>Questão1<<<
+
+function informacoesPessoais (nome, idade, cidade, profissao) {
+ const informacoes = `Eu sou ${nome} tenho ${idade} anos, moro em ${cidade} e sou ${profissao}`
+ console.log(informacoes)
+}
+
+informacoesPessoais("ithalo", 18, "Alta Floresta", "Estudante" */
+
+/* >>>Questão2<<<
+a.
+function doisNumeros (_primeiroNum, _segundoNum) {
+ const resultado = _primeiroNum + _segundoNum
+ return resultado 
+}
+
+const resultadoDaSoma = doisNumeros(10, 5)
+console.log(doisNumeros(10,5)) */
+
+/* b.
+/* function doisNumeros (_primeiroNum, _segundoNum) {
+  const resultado = _primeiroNum >= _segundoNum
+  return resultado
+}
+
+const resultadoDaSoma = doisNumeros(10, 5)
+console.log(doisNumeros(10, 5)) */ 
+
+/* c.
+function doisNumeros (_primeiroNum, _segundoNum) {
+  const resultado = _primeiroNum % _segundoNum === 0
+  return resultado 
+}
+ const resultadoDaSomam = doisNumeros(10, 5)
+ console.log(doisNumeros(10, 5)) */
+ 
+/*  d.
+ /* function umaMensagem (texto) {
+   const mensagem = texto
+   return mensagem
+ }
+
+ const aMensagem = umaMensagem("Gosto de Batata")
+ console.log(umaMensagem ("Gosto de batata") (texto.length()). texto.toUpperCase()) //não consegui fazer esse exercicio */ 
+
+ //tive alguns atrasados na parte da tarde e não tive tempo de conseguir resolver o exericio 3 ._.
