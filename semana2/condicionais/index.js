431df0b0f01fac31442cc9ca3878fefeebb8d88c
Ithalo Nimai <ithalodes@gmail.com>
2021-08-17 16:00:23 -0400

    Exercicios de condicionais

---
+++ semana2/condicionais/index.js
@@ -0,0 +1,53 @@
+//>>>Exercicios interpretação<<<
+//1.
+/* a. irá testar se o número digitado será par ou não
+b. para números pares
+c. para números impar
+
+2. 
+a. para informar no console o preço da fruta
+b. O preço da fruta, Maçã é, R$2.25
+c. O preço da fruta  pêra  é  R$  5
+
+3.
+a. pedindo para que o usuário digite um número
+b. pra 10 iria imprimir: Esse número passou no teste. Não iria imprimir nada 
+c. iria dizer que a variável "mensagem" não está definida. Let mensagem está 
+na no espoco da função, ou seja não tem como tirar ele de la com os comandos
+que está nesse código.
+*/
+
+
+/*>>>Exercícios de escrita de código<<<
+1.
+const idadeUsuario = Number(prompt("Qual sua idade?"))
+
+if (idadeUsuario >= 18) {
+  console.log("Você pode dirigir.")
+}
+  else{
+    console.log("Você não pode dirigir.")
+  }
+*/
+// 4.
+/*const valorIngresso = Number(prompt("Qual o preço do ingresso?"))
+
+let genero
+
+if (valorIngresso < 15) {
+  console.log("Bom filme!")
+}
+  else{
+    console.log("Escolha outro filme :(")
+  }
+*/
+
+const generoFilme = prompt("Qual genêro do filme você irá assistir?")
+let genero = "Fantasia"
+
+if (generoFilme = genero) {
+  console.log("Bom filme")
+}
+  else{
+    console.log("Escolha outro filme :(")
+  }//nao consegui terminar e nem entender muito bem.
