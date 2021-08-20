ea8a1e43f785ca32e2ccab83873e5943b17c5e02
Ithalo Nimai <ithalodes@gmail.com>
2021-08-20 17:23:59 -0400

    Adicionei Projeto-semana2

---
+++ semana2/funcoes-de-array/index.js
@@ -0,0 +1,15 @@
+/* >>>Exercicio interpretação<<<
+1.
+*/
+
+const usuarios = [
+  { nome: "Amanda Rangel", apelido: "Mandi" },
+  { nome: "Laís Petra", apelido: "Laura" },
+  { nome: "Letícia Chijo", apelido: "Chijo" },
+]
+
+const novoArrayB = usuarios.map((item, index, array) => {
+   return item.nome
+})
+
+console.log(novoArrayB)
